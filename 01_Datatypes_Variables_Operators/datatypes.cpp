a78558d16b050a31d684f1745e0df61007d53d28
Bhakti Kavathiya <bhaktikavathiya5@gmail.com>
2026-09-09 00:09:56 +0530

    Add datatypes and variables programs

---
+++ 01_Datatypes_Variables_Operators/datatypes.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 123;
+    cout <<a<< endl;
+
+    char b = 'v';
+    cout <<b<< endl;
+
+    bool bl = true;
+    cout <<bl<< endl;
+
+    float f = 1.2;
+    cout <<f<< endl;
+
+    double d = 1.23;
+    cout <<d<< endl;
+
+    int size = sizeof(d);
+    cout<<"Size of a is :"<< size << endl;
+
+    int e = 'e';
+    cout << e << endl;
+
+    char ch1 = 123456;
+    cout << ch1 << endl;
+
+    unsigned int p = -112;
+    cout << p << endl;
+
+}
+
