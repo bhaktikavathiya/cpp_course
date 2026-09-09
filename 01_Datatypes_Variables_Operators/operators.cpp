23134081b269836f98301d2f9ae2ac6dcfaeda33
Bhakti Kavathiya <bhaktikavathiya5@gmail.com>
2026-09-09 23:57:26 +0530

    Add operators practice

---
+++ 01_Datatypes_Variables_Operators/operators.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // arithematic operators
+    float f = 2.0/5;
+    cout << f << endl;
+    cout << 2.0/5 << endl;
+
+    double d = 2.00/5;
+    cout << d << endl;
+
+    int a = 2;
+    int b = 3;
+
+    bool first = (a==b);
+    cout << first << endl;
+
+    bool second = (a>b);
+    cout << second << endl;
+
+    bool third = (a<b);
+    cout << third << endl;
+
+    bool fourth = (a>=b);
+    cout << fourth << endl;
+
+    bool fifth = (a<=b);
+    cout << fifth << endl;
+
+    bool sixth = (a!=b);
+    cout << sixth << endl;
+
+    //logical operators
+    int n = 0;
+    cout << !a << endl;
+        
+}
