59c2f943561ec40990f1d63e77181038cc2e8913
Bhakti Kavathiya <bhaktikavathiya5@gmail.com>
2026-09-19 22:00:04 +0530

    Add number pattern question

---
+++ 03_Loops/patterns12.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int i = 1;
+    while(i <= n){
+        int j = 1, space = 1;
+        while(space <= n - i){
+            cout << "  "; //two spaces 
+            space = space + 1;
+        }
+        while(j <= i){
+            cout << j << " ";
+            j = j + 1;
+        }
+        while(j >= 1){
+            cout << j << " ";
+            j = j - 1;
+        }
+        i = i + 1;
+        cout << endl;
+    }
+    return 0;
+}
